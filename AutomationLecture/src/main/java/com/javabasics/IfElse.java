a954bb16c877182b4b01fcd81dc7ae53fb63e117
ziaha <ziaha@Zainab-PC>
2020-02-03 13:45:57 -0500

    Pushing Automation Lecture to GittHub cloud

---
+++ AutomationLecture/src/main/java/com/javabasics/IfElse.java
@@ -0,0 +1,28 @@
+package com.javabasics;
+
+public class IfElse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(2+2);
+		addition();
+		int x = 10;
+		int y = 20;
+		
+		if(y > x) {
+			System.out.println("20 is greater than 10");
+		}
+		else {
+			System.out.println("20 is not greater than 10");
+		}
+
+	}
+	public static void addition() {
+		System.out.println(5+5);		
+	}
+
+	public static void subtraction() {
+		System.out.println(15+5);		
+	}
+	
+}
